9750c5789647e59728405041257d8835cadd253f
ValyaGrachov <goust190703@gmail.com>
2024-01-25 22:03:56 +0300

    Add Identity

---
+++ TicTacToe_Backend/Core/Domain/ViewModels/UserLoginVm.cs
@@ -0,0 +1,8 @@
+﻿namespace Domain.ViewModels;
+
+public class UserLoginVm
+{
+    public string? UserName { get; set; }
+    
+    public string? Password { get; set; }
+}
---
+++ TicTacToe_Backend/TicTacToe_Backend/Helpers/Authorization/JwtGenerator.cs
@@ -0,0 +1,43 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Domain.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+
+namespace TicacToe_Backend.Helpers.Authorization;
+
+public class JwtGenerator: IJwtGenerator
+{
+    private readonly IConfiguration _configuration;
+
+    public JwtGenerator(IConfiguration configure)
+    {
+        _configuration = configure;
+    }
+
+    public string CreateToken(IdentityUser user)
+    {
+        var jwtTokenHandler = new JwtSecurityTokenHandler();
+
+        var key = Encoding.UTF8.GetBytes(_configuration["JWT:Key"]!);
+
+        var tokenDescriptor = new SecurityTokenDescriptor()
+        {
+            Subject = new ClaimsIdentity(new []
+            {
+                new Claim("Id", user.Id),
+                new Claim("UserName", user.UserName),
+            }),
+            
+            Expires = DateTime.Now.AddHours(1),
+            SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key),
+                SecurityAlgorithms.HmacSha256)
+        };
+        var token = jwtTokenHandler.CreateToken(tokenDescriptor);
+        var jwtToken = jwtTokenHandler.WriteToken(token);
+
+        return jwtToken;
+    }
+}
