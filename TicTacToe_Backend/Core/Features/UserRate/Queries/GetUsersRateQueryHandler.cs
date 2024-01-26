7c3ce0456354295b3fdcb28ac0af57a433080e7e
ValyaGrachov <goust190703@gmail.com>
2024-01-26 23:24:27 +0300

    Start Rates

--- TicTacToe_Backend/Core/Features/UserRate/Queries/GetUsersRateQueryHandler.cs
+++ TicTacToe_Backend/Core/Features/UserRate/Queries/GetUsersRateQueryHandler.cs
@@ -1,10 +1,18 @@
-﻿using Shared.CQRS;
+﻿using DataAccess;
+using Shared.CQRS;
 using Shared.Results;
 
 namespace Features.UserRate.Queries;
 
 public class GetUsersRateQueryHandler: IQueryHandler<GetUsersRateQuery, IEnumerable<UsersRateDto>>
 {
+    private readonly IUserRepository _userRepository;
+
+    public GetUsersRateQueryHandler(IUserRepository userRepository)
+    {
+        _userRepository = userRepository;
+    }
+
     public Task<Result<IEnumerable<UsersRateDto>>> Handle(GetUsersRateQuery request, CancellationToken cancellationToken)
     {
         throw new NotImplementedException();
